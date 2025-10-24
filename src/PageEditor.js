be9926434241ef084738149a84cd33d25386a382
yamainu-2525MT194 <yama7765yukib@gmail.com>
2025-10-24 17:07:39 +0900

    Style3: Improve UI layout of PageEditor

--- src/PageEditor.js
+++ src/PageEditor.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
-import { Container, Typography, Box, TextField, Button, Paper, Grid, CircularProgress } from '@mui/material';
+import { Typography, Box, TextField, Button, Paper, Grid, CircularProgress } from '@mui/material';
 
 function PageEditor() {
   const navigate = useNavigate();
